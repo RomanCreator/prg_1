7cd31b79e5b0c6ddc5b33a3c7e2de4247cc2a8b1
lr <roman-991@ya.ru>
2016-07-10 00:17:31 +0300

    Закончен плагин для обратного отсчета при удалении сущности. Работает только для конструкции с формой.

--- resources/assets/js/backend/countdown.js
+++ resources/assets/js/backend/countdown.js
@@ -4,130 +4,89 @@
 +function ($) {
     'use strict';
 
+    var CountdownElemForm = function (element, options) {
+        this.$element = $(element);
+        this.CurrentOption = this.getOptions (options);
+        this.$form = this.$element.closest('form');
+        this.intervalId = null;
+        this.countdownState = 'start';
+        this.innerHtml = null;
+
+        var self = this;
+        this.$form.bind('submit.countdown', function () {
+            return self.sendForm();
+        });
+
+        this.$element.bind('click.countdown', function () {
+            if (self.countdownState == 'start') {
+                self.countdownState = 'proccess';
+                self.innerHtml = self.$element.html();
+                self.$element.html(self.CurrentOption.countdownTime);
+                self.intervalId = setInterval(function () {
+                    self.action.call(self);
+                }, self.CurrentOption.countdownIntervalSec*1000);
+            }
+
+            if (self.countdownState == 'end') {
+                return true;
+            }
+            return false;
+        });
+    };
+
+    /**
+     * Сам непосредственный экшн для отсчета
+     */
+    CountdownElemForm.prototype.action = function () {
+        var time = parseInt(this.$element.html());
+        if (time > 0) {
+            time--;
+            this.$element.html(time);
+        } else {
+            this.$element.html(this.innerHtml);
+            this.countdownState = 'end';
+            clearInterval(this.intervalId);
+        }
+    }
+
+    /**
+     * Разршаем отправку формы тогда и только тогда, когда
+     * заканчивается обратный отсчет, до этого форму не отрпавляем
+     * @returns {boolean}
+     */
+    CountdownElemForm.prototype.sendForm = function () {
+        if (this.countdownState !== 'end') {
+            return false;
+        }
+
+        return true;
+    }
+
+    CountdownElemForm.prototype.options = {
+        countdownTime: 3,
+        countdownIntervalSec: 1,
+    };
+
+    CountdownElemForm.prototype.getOptions = function (options) {
+        if (options && (typeof options) == 'Object') {
+            for (var key in this.options) {
+                if (!options[key]) {
+                    options[key] = this.options[key];
+                };
+            }
+
+            return options;
+        }
+
+        return this.options;
+    };
+
     var Countdown = function (options) {
         //вернуть надо объект jQuery
         return this.each(function () {
-
-            Countdown.init(this, options);
+            new CountdownElemForm(this, options);
         });
     };
-
-    Countdown.prototype.VERSION = '1.0.0';
-
-    /**
-     * Дефолтные настройки плагина обратного отсчета
-     *
-     * параметр type может быть задан значением link в таком случае
-     * развитие инициализации идет по сценарию ссылки с обратным отсчетом
-     * иначе по кнопке submit в форме
-     *
-     * параметр countdownType задается в секундах
-     *
-     * callback должен быть функцией или bool
-     * переменной в значении false если тело функции не
-     * установлено
-     *
-     * @type {{type: string, countdownTime: number, callback: boolean}}
-     */
-    Countdown.prototype.DEFAULTS = {
-        type: 'form',
-        countdownTime: 3,
-        callback: false,
-    };
-
-    /**
-     * Основная функция инициализации обратного отсчета
-     * @param elem
-     */
-    Countdown.prototype.init = function (elem, options) {
-        var OptionsElem = this.getOption(options);
-
-        var $elem = $(elem);
-        $elem.data('countdownState', 'start');
-
-        /* читаем опции, и идем по разным сценариям инициализации */
-        switch (OptionsElem.type) {
-            case 'form':
-                /* перехватываем submit у ближайшй к элементу форме */
-                /* внутри смотрим на текущее состояние countdownState элемента */
-                /* если оно финально то отправляем форму в противном случае ничего не делаем */
-                    var $form = $elem.closest('form');
-                    $form.bind('submit.countdown', this.formSubmit);
-                break;
-            case 'link':
-                break;
-        }
-
-        $elem.bind('click.countdown', function () {
-
-        });
-
-    };
-
-    /**
-     * Функция для обработки формы
-     * @returns {boolean}
-     */
-    Countdown.prototype.formSubmit = function () {
-        var state = $(this).find('[data-toggle="countdown"]').data('countdownState');
-        switch (state) {
-            case 'end':
-                return true;
-                break;
-            default:
-                return false;
-        }
-    }
-
-    Countdown.prototype.action = function () {
-        /**
-         * Тут обратный отсчет
-         */
-    }
-
-    /**
-     * Возвращает дефолные и переданные опции слитые в
-     * общие
-     *
-     * @param options
-     */
-    Countdown.prototype.getOption = function (options) {
-        if (!options.type) {
-            options.type = this.DEFAULTS.type;
-        } else {
-            switch (options.type) {
-                case 'form':
-                case 'link':
-                    break;
-                default:
-                    throw new Error('Не верно указан тип элемента обратного отсчета');
-            }
-        }
-
-        if (!options.countdownTime) {
-            options.countdownTime = this.DEFAULTS.countdownTime;
-        } else {
-            if (isNaN(options.countdownTime)) {
-                throw new Error('Не верно указано время обратного отсчета');
-            }
-        }
-
-        if (!options.callback) {
-            options.callback = this.DEFAULTS.callback;
-        } else {
-            switch (typeof options.callback) {
-                case 'function':
-                case 'boolean':
-                    break;
-                default:
-                    throw new Error('CallBack имеет значение не разрешенного типа');
-            }
-        }
-
-        return options;
-    }
-
-
 
 
     var old = $.fn.countdown;
